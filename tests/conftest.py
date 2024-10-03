7450af61d52de6640860d9fc811d32c1327f3454
Alex Warren <alexander.ch.warren@gmail.com>
2024-10-03 15:44:54 -0400

    Pass conftest.py through black

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
 import pytest
 
+
 def pytest_addoption(parser):
     parser.addoption("--keep-test-dir", action="store_true")
+
 
 @pytest.fixture
 def project_path(request: pytest.FixtureRequest) -> str:
